0ef7f60c7dd3b59e955f2472f0d316f8b80faafa
kaylamaedizon <kaylamaedizon@gmail.com>
2017-11-15 17:33:44 +0800

    Equipped with packages

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './../assets/logo.svg';
 import mat from 'materialize-css/dist/css/materialize.css';
-import './App.css';
+import './../css/App.css';
 
 console.log(mat);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App row">
+        <header className="App-header row">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to ACSS Node-React Guide</h1>
         </header>
-
       </div>
     );
   }
