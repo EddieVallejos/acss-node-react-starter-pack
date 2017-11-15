68f4ed0a00a218a911efa8e1bb10fb1eb09cb78c
kaylamaedizon <kaylamaedizon@gmail.com>
2017-11-15 17:43:24 +0800

    Fix in schema

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import logo from './../assets/logo.svg';
-import mat from 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js';
 import './../css/App.css';
 
-console.log(mat);
-
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      results: ''
+    }
+  }
+
   render() {
     return (
       <div className="App row">
@@ -13,6 +20,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to ACSS Node-React Guide</h1>
         </header>
+        <div className="row">
+          <span>SEARCH: </span>
+          <div class="row">
+            <div className="col s4"/>
+            <div class="input-field col s2">
+              <input id="name" type="text" class="validate"/>
+              <label for="name">Name</label>
+            </div>
+            <div class="input-field col s2">
+              <button class="btn waves-effect waves-light"name="action">Submit
+                <i class="material-icons right">send</i>
+              </button>
+            </div>            
+            <div className="col s4"/>            
+          </div>
+        </div>
       </div>
     );
   }
